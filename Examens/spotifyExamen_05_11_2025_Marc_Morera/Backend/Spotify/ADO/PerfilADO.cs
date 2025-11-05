68f8ad317f6c1e2b087719db7aef1cd09ba28197
mmorerap <marcmoreraprat@gmail.com>
2025-11-05 18:15:08 +0100

    18:15

---
+++ Examens/spotifyExamen_05_11_2025_Marc_Morera/Backend/Spotify/ADO/PerfilADO.cs
@@ -0,0 +1,108 @@
+using Microsoft.Data.SqlClient;
+using Spotify.Model;
+using Spotify.Services;
+
+
+namespace Spotify.Repository;
+
+class PerfilADO
+{
+    public static void Insert(DatabaseConnection dbConn, Song song)
+    {
+        dbConn.Open();
+
+        string sql = @"INSERT INTO Perfils (Id, Name, Descripcio, Estat, User_ID)
+                        VALUES (@Id, @Name, @Descripcio, @Estat, @User_ID)";
+
+        using SqlCommand cmd = new SqlCommand(sql, dbConn.sqlConnection);
+        cmd.Parameters.AddWithValue("@ID", song.Id);
+        cmd.Parameters.AddWithValue("@Name", song.Name);
+        cmd.Parameters.AddWithValue("@Descripcio", song.Id);
+        cmd.Parameters.AddWithValue("@Estat", song.Name);
+        cmd.Parameters.AddWithValue("@User_ID", song.Id);
+      
+        cmd.ExecuteNonQuery();
+        dbConn.Close();
+    }
+    public static List<Song> GetAll(DatabaseConnection dbConn)
+    {
+        List<Song> songs = new();
+
+        dbConn.Open();
+        string sql = "SELECT Id, Name FROM Songs";
+
+        using SqlCommand cmd = new SqlCommand(sql, dbConn.sqlConnection);
+        using SqlDataReader reader = cmd.ExecuteReader();
+
+        while (reader.Read())
+        {
+            songs.Add(new Song
+            {
+                Id = reader.GetGuid(0),
+                Name = reader.GetString(1),
+            });
+        }
+        dbConn.Close();
+        return songs;
+    }
+
+    public static Song? GetById(DatabaseConnection dbConn, Guid Id)
+    {
+        dbConn.Open();
+        string sql = "SELECT Id, Name FROM Songs WHERE Id = @Id";
+
+        using SqlCommand cmd = new SqlCommand(sql, dbConn.sqlConnection);
+        cmd.Parameters.AddWithValue("@Id", Id);
+
+        using SqlDataReader reader = cmd.ExecuteReader();
+        Song? song = null;
+
+        if (reader.Read())
+        {
+            song = new Song
+            {
+                Id = reader.GetGuid(0),
+                Name = reader.GetString(1),
+            };
+        }
+
+        dbConn.Close();
+        return song;
+    }
+
+    public static void Update(DatabaseConnection dbConn, Song song)
+    {
+        dbConn.Open();
+
+        string sql = @"UPDATE Songs 
+                        SET Id = @Id,
+                        Name = @Name
+                        WHERE Id = @Id";
+        using SqlCommand cmd = new SqlCommand(sql, dbConn.sqlConnection);
+        cmd.Parameters.AddWithValue("@Id", song.Id);
+        cmd.Parameters.AddWithValue("@Name", song.Name);
+
+        int rows = cmd.ExecuteNonQuery();
+
+        // Console.WriteLine($"{rows} fila actualitzada.");
+
+        cmd.ExecuteNonQuery();
+        dbConn.Close();
+    }
+
+    public static bool Delete(DatabaseConnection dbConn, Guid Id)
+    {
+        dbConn.Open();
+
+        string sql = @"DELETE FROM Songs WHERE Id = @Id";
+
+        using SqlCommand cmd = new SqlCommand(sql, dbConn.sqlConnection);
+        cmd.Parameters.AddWithValue("@Id", Id);
+
+        int rows = cmd.ExecuteNonQuery();
+
+        dbConn.Close();
+
+        return rows > 0;
+    }
+}
--- Examens/spotifyExamen_05_11_2025_Marc_Morera/Backend/Spotify/models/Media.cs
+++ Examens/spotifyExamen_05_11_2025_Marc_Morera/Backend/Spotify/models/Media.cs
@@ -5,7 +5,5 @@ public class Media
     public Guid Id { get; set; }
     
     public Guid Song_Id { get; set; }
-    public string Url { get; set; } = "";
-    public string ImagePath { get; set; } = "";
-    
+    public string Url { get; set; } = "";    
 }
