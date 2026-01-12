1c826afd4b0d6f4e274bb0be16f648c4ba3e5527
mmorerap <marcmoreraprat@gmail.com>
2026-01-12 22:07:35 +0100

    CarritoComprasADO acabat v1.0

--- Acces-a-Dades/Shop/Model/CarritoCompra.cs
+++
@@ -1,9 +0,0 @@
-namespace dbdemo.Model;
-
-public class CarritoCompras
-{
-    public Guid Id { get; set; }
-    public string Nom { get; set; } = "";
-    public string Descripcio { get; set; } = "";
-    
-}
--- Acces-a-Dades/Shop/Repository/CarritoComprasADO.cs
+++ Acces-a-Dades/Shop/Repository/CarritoComprasADO.cs
@@ -10,20 +10,19 @@ class CarritoComprasADO
 {
    
 
-    public static void Insert(DatabaseConnection dbConn,CarritoCompras carritoCompras)
+    public static void Insert(DatabaseConnection dbConn, CarritoCompras carritoCompras)
     {
 
         dbConn.Open();
 
-        string sql = @"INSERT INTO CarritoCompras (Id, Nom)         
-                        VALUES (@Id, @Nom)";            
+        string sql = @"INSERT INTO CarritoCompras (Id, Nom, Descripcio)     
+                        VALUES (@Id, @Nom, @Descripcio)";            
 
         using SqlCommand cmd = new SqlCommand(sql, dbConn.sqlConnection);
         cmd.Parameters.AddWithValue("@Id", carritoCompras.Id);
         cmd.Parameters.AddWithValue("@Nom", carritoCompras.Nom);
-
-        int rows = cmd.ExecuteNonQuery();
-        Console.WriteLine($"{rows} fila inserida.");
+        cmd.Parameters.AddWithValue("@Descripcio", carritoCompras.Descripcio);
+        
         dbConn.Close();
     }
 
@@ -32,7 +31,7 @@ class CarritoComprasADO
         List<CarritoCompras> carritoCompras = new();
 
         dbConn.Open();
-        string sql = "SELECT Id, Nom FROM CarritoCompras";
+        string sql = "SELECT Id, Nom, Descripcio FROM CarritoCompras";
 
         using SqlCommand cmd = new SqlCommand(sql, dbConn.sqlConnection);
         using SqlDataReader reader = cmd.ExecuteReader();
@@ -43,6 +42,7 @@ class CarritoComprasADO
             {
                 Id = reader.GetGuid(0),
                 Nom = reader.GetString(1),
+                Descripcio = reader.GetString(2),
             });
         }
 
@@ -67,10 +67,49 @@ class CarritoComprasADO
             {
                 Id = reader.GetGuid(0),
                 Nom = reader.GetString(1),
+                Descripcio = reader.GetString(2),
             };
         }
 
         dbConn.Close();
         return carritoCompras;
+    }
+
+
+     public static void Update(DatabaseConnection dbConn, CarritoCompras carritoCompras)
+    {
+        dbConn.Open();
+
+        string sql = @"UPDATE CarritoCompras 
+                        SET
+                        Id = @Id,
+                        Nom = @Nom,
+                        Descripcio = @Descripcio
+                        WHERE Id = @Id";
+                       
+
+
+        using SqlCommand cmd = new SqlCommand(sql, dbConn.sqlConnection);
+        cmd.Parameters.AddWithValue("@Id", carritoCompras.Id);
+        cmd.Parameters.AddWithValue("@Nom", carritoCompras.Nom);
+        cmd.Parameters.AddWithValue("@Descripcio", carritoCompras.Descripcio);
+
+        dbConn.Close();
+    }
+
+    public static bool Delete(DatabaseConnection dbConn, Guid Id)
+    {
+        dbConn.Open();
+
+        string sql = @"DELETE FROM CarritoCompras WHERE Id = @Id";
+
+        using SqlCommand cmd = new SqlCommand(sql, dbConn.sqlConnection);
+        cmd.Parameters.AddWithValue("@Id", Id);
+
+        int rows = cmd.ExecuteNonQuery();
+
+        dbConn.Close();
+
+        return rows > 0;
     }
 }
