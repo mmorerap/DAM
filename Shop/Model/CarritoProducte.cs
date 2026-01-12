f336918590c5a9bd33a1fb6341e3aa8339ba7557
mmorerap <marcmoreraprat@gmail.com>
2026-01-12 19:44:53 +0100

    -MODELS ACABATS v1.0 - Creacio Model CarritoProducte

---
+++ Shop/Model/CarritoProducte.cs
@@ -0,0 +1,9 @@
+namespace dbdemo.Model;
+
+public class CarritoProducte
+{
+    public Guid Id { get; set; }
+    public string ID_CARR { get; set; } = "";
+    public string ID_PROD { get; set; } = "";
+    public string Quantitat { get; set; } = "";
+}
