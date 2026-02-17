ee9b5f3b2c8d7092a3c29bfcbae8b1ddbc93eb81
mmorerap <marcmoreraprat@gmail.com>
2026-02-17 16:03:10 +0100

    BDD adeptada a Domain

--- Acces-a-Dades/Shops/Shop_DAI_DOMAIN/Shop/db.sql
+++ Acces-a-Dades/Shops/Shop_DAI_DOMAIN/Shop/db.sql
@@ -1,51 +1,100 @@
-CREATE TABLE Familia (
-    ID UNIQUEIDENTIFIER DEFAULT NEWID() PRIMARY KEY,
-    Nom NVARCHAR(20) NOT NULL,
-    Descripcio NVARCHAR(MAX)
+-- DROP SCHEMA dbo;
+
+CREATE SCHEMA dbo;
+-- b1botiga.dbo.Client definition
+
+-- Drop table
+
+-- DROP TABLE b1botiga.dbo.Client;
+
+CREATE TABLE b1botiga.dbo.Client (
+	ID uniqueidentifier DEFAULT newid() NOT NULL,
+	Nom nvarchar(100) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+	DNI nvarchar(20) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+	ADDR nvarchar(255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	CONSTRAINT PK__Client__3214EC2700913ECE PRIMARY KEY (ID)
 );
 
-CREATE TABLE Producte (
-    ID UNIQUEIDENTIFIER DEFAULT NEWID() PRIMARY KEY,
-    CODI CHAR(8) NOT NULL,
-    Descripcio NVARCHAR(MAX),
-    Preu DECIMAL(10, 2),
-    Descompte DECIMAL(10, 2),
-    tipus INT,
-    id_tipus UNIQUEIDENTIFIER,
-    FOREIGN KEY(id_tipus) REFERENCES Familia(ID)
+
+-- b1botiga.dbo.Familia definition
+
+-- Drop table
+
+-- DROP TABLE b1botiga.dbo.Familia;
+
+CREATE TABLE b1botiga.dbo.Familia (
+	ID uniqueidentifier DEFAULT newid() NOT NULL,
+	Nom nvarchar(20) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+	Descripcio nvarchar(MAX) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	CONSTRAINT PK__Familia__3214EC279A7ACC31 PRIMARY KEY (ID)
 );
 
-CREATE TABLE CarritoCompra (
-    ID UNIQUEIDENTIFIER DEFAULT NEWID() PRIMARY KEY,
-    nom VARCHAR(10) NOT NULL
+
+-- b1botiga.dbo.CarritoCompras definition
+
+-- Drop table
+
+-- DROP TABLE b1botiga.dbo.CarritoCompras;
+
+CREATE TABLE b1botiga.dbo.CarritoCompras (
+	ID uniqueidentifier DEFAULT newid() NOT NULL,
+	nom varchar(100) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+	Descripcio varchar(100) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	ID_CLI uniqueidentifier NULL,
+	DataCompra date NULL,
+	CONSTRAINT PK__CarritoC__3214EC2710D248FD PRIMARY KEY (ID),
+	CONSTRAINT FK__CarritoCo__ID_CL__1AD3FDA4 FOREIGN KEY (ID_CLI) REFERENCES b1botiga.dbo.Client(ID)
 );
 
-CREATE TABLE CarritoProducte (
-    ID UNIQUEIDENTIFIER DEFAULT NEWID() PRIMARY KEY,
-    ID_CARR UNIQUEIDENTIFIER,
-    ID_PROD UNIQUEIDENTIFIER,
-    Quantitat INT NOT NULL,
-    FOREIGN KEY(ID_CARR) REFERENCES CarritoCompra(ID),
-    FOREIGN KEY(ID_PROD) REFERENCES Producte(ID)
+
+-- b1botiga.dbo.Product definition
+
+-- Drop table
+
+-- DROP TABLE b1botiga.dbo.Product;
+
+CREATE TABLE b1botiga.dbo.Product (
+	ID uniqueidentifier DEFAULT newid() NOT NULL,
+	Code nvarchar(8) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+	Descripcio nvarchar(MAX) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	Price decimal(10,2) NULL,
+	Descompte decimal(10,2) NULL,
+	IdFamilia uniqueidentifier NULL,
+	Name varchar(30) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+	CONSTRAINT PK__Producte__3214EC27900FDAD7 PRIMARY KEY (ID),
+	CONSTRAINT FK__Producte__id_tip__0D7A0286 FOREIGN KEY (IdFamilia) REFERENCES b1botiga.dbo.Familia(ID)
 );
 
-CREATE TABLE Client (
-    ID UNIQUEIDENTIFIER DEFAULT NEWID() PRIMARY KEY,
-    Nom NVARCHAR(100) NOT NULL,
-    DNI NVARCHAR(20) NOT NULL,
-    ADDR NVARCHAR(255)
+
+-- b1botiga.dbo.RegistrePreus definition
+
+-- Drop table
+
+-- DROP TABLE b1botiga.dbo.RegistrePreus;
+
+CREATE TABLE b1botiga.dbo.RegistrePreus (
+	ID uniqueidentifier DEFAULT newid() NOT NULL,
+	ID_PROD uniqueidentifier NOT NULL,
+	Price decimal(10,2) NOT NULL,
+	DataRegistre date NULL,
+	CONSTRAINT PK_RegistrePreus PRIMARY KEY (ID),
+	CONSTRAINT FK_RegistrePreus_Product FOREIGN KEY (ID_PROD) REFERENCES b1botiga.dbo.Product(ID)
 );
 
-CREATE TABLE RegistrePreus (
-    ID UNIQUEIDENTIFIER DEFAULT NEWID() PRIMARY KEY,
-    ID_PROD UNIQUEIDENTIFIER NOT NULL,
-    Data_Price DATE NOT NULL,
-    Price DECIMAL(10, 2) NOT NULL,
-    FOREIGN KEY(ID_PROD) REFERENCES Product(ID)
+
+-- b1botiga.dbo.CarritoProducte definition
+
+-- Drop table
+
+-- DROP TABLE b1botiga.dbo.CarritoProducte;
+
+CREATE TABLE b1botiga.dbo.CarritoProducte (
+	ID uniqueidentifier DEFAULT newid() NOT NULL,
+	ID_CARR uniqueidentifier NULL,
+	ID_PROD uniqueidentifier NULL,
+	Quantitat decimal(10,0) NOT NULL,
+	Price decimal(38,0) NULL,
+	CONSTRAINT PK__CarritoP__3214EC27A77FBC32 PRIMARY KEY (ID),
+	CONSTRAINT FK__CarritoPr__ID_CA__114A936A FOREIGN KEY (ID_CARR) REFERENCES b1botiga.dbo.CarritoCompras(ID),
+	CONSTRAINT FK__CarritoPr__ID_PR__123EB7A3 FOREIGN KEY (ID_PROD) REFERENCES b1botiga.dbo.Product(ID)
 );
-
-ALTER TABLE CarritoCompras 
-ADD ID_CLI UNIQUEIDENTIFIER;
-
-ALTER TABLE CarritoCompras
-ADD FOREIGN KEY(ID_CLI) REFERENCES Client(ID);
